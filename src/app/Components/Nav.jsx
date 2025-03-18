512271f1c5126b510d7b2fcaa71e2094036530cb
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-18 21:24:16 +0700

    Test Update on diff lap

--- src/app/Components/Nav.jsx
+++ src/app/Components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
         <div>
             {/* Navigation Bar */}
             <nav className="bg-gray-900 text-white py-4 px-8 flex justify-between items-center">
-                <h1 className="text-xl font-bold animate-fade-in-down gradient-text ">ScrollGallery </h1>
+                <h1 className="text-xl font-bold animate-fade-in-down gradient-text ">ScrollGallery</h1>
                 <ul className="flex space-x-4 animate-fade-in-down">
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/">About</Link></li>
