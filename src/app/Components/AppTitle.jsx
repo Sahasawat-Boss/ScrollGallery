06cdaa9a3b9ce0dc4cede5c865a9532527b54e59
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-18 07:37:11 +0700

    UPdate TItle Comps

--- src/app/Components/AppTitle.jsx
+++ src/app/Components/AppTitle.jsx
@@ -6,20 +6,22 @@ const AppTitle = () => {
     const [isVisible, setIsVisible] = useState(true);
 
     return (
-        <header className="text-center py-6 bg-gray-900 text-white ">
-            <h1 className="text-4xl font-bold animate-fade-in-up">ScrollGallery</h1>
+        <header className="text-center py-3 bg-gray-900 text-white ">
+            <h1 className="text-4xl font-bold animate-fade-in-up mt-4">ScrollGallery</h1>
             {isVisible && (
-                <p className="text-lg mt-2 max-w-2xl mx-auto animate-fade-in-up">
+                <p className="text-lg mt-3 max-w-2xl mx-auto animate-fade-in-up">
                     ScrollGallery is an infinite scrolling image gallery that lets you explore endless pictures seamlessly. Enjoy a smooth and immersive browsing experience with high-quality images loaded dynamically.
                 </p>
             )}
-            <div className="flex justify-center">
+            <div className="flex justify-center mt-2">
                 <button
-                    className="mt-4 px-2 py-1 text-white rounded transition flex items-center hover:bg-gray-600 gap-2"
+                    className="px-3 py-1.5 text-white rounded transition focus:outline-none hover:bg-gray-600 flex items-center gap-2 relative group"
                     onClick={() => setIsVisible(!isVisible)}
                 >
-                    {isVisible ? <FiEyeOff size={20} /> : <FiEye size={20} />}
-                    {isVisible ? "" : ""} {/*---If wanna add some text later---*/}
+                    {isVisible ? <FiEyeOff size={16} /> : <FiEye size={16} />}
+                    <span className="absolute left-[45px] hidden group-hover:block bg-gray-800 text-white text-sm px-2 py-1 rounded transition">
+                        {isVisible ? "Hide Description" : "Show Description"}
+                    </span>
                 </button>
             </div>
         </header>
